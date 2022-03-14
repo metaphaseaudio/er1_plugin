9728de1d9cd87d536fda6ef56e272b0c6c0113a8
Matt Zapp <info@metaphaseaudio.com>
2022-03-13 22:35:41 -0400

    upgrading to c++17

--- src/er1/guts/juce_synth/ER1VoiceController.cpp
+++ src/er1/guts/juce_synth/ER1VoiceController.cpp
@@ -37,14 +37,14 @@ void ER1Voice::renderNextBlock(juce::AudioBuffer<float>& outputBuffer, int start
     if (auto* voice_params = static_cast<ER1Sound*> (getCurrentlyPlayingSound().get()))
     {
         // Plus one here to align with the options
-        m_Voice.setWaveType(static_cast<meta::ER1::Oscillator::WaveType>(voice_params->osc.oscType->getIndex() + 1));
-        m_Voice.setModulationType(static_cast<meta::ER1::Voice::ModType>(voice_params->osc.modType->getIndex()));
-        m_Voice.setModulationSpeed(voice_params->osc.modSpeed->get());
-        m_Voice.setModulationDepth(voice_params->osc.modDepth->get());
-        m_Voice.setPitch(voice_params->osc.pitch->get());
-        m_Voice.level = voice_params->amp.level->get();
-        m_Voice.pan = voice_params->amp.pan->get();
-        m_Voice.setDecay(voice_params->amp.decay->get());
+//        m_Voice.setWaveType(static_cast<meta::ER1::Oscillator::WaveType>(voice_params->osc.oscType->getIndex() + 1));
+//        m_Voice.setModulationType(static_cast<meta::ER1::Voice::ModType>(voice_params->osc.modType->getIndex()));
+//        m_Voice.setModulationSpeed(voice_params->osc.modSpeed->get());
+//        m_Voice.setModulationDepth(voice_params->osc.modDepth->get());
+//        m_Voice.setPitch(voice_params->osc.pitch->get());
+//        m_Voice.level = voice_params->amp.level->get();
+//        m_Voice.pan = voice_params->amp.pan->get();
+//        m_Voice.setDecay(voice_params->amp.decay->get());
 
         m_Voice.processBlock(
             outputBuffer.getArrayOfWritePointers()
