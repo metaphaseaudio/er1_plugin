a61870006f547e7052abcc6d99c18c95ba5a3252
Matt Zapp <info@metaphaseaudio.com>
2024-08-03 20:38:11 -0400

    windows dev gui elements and plugins install

---
+++ scripts/install_extras_win.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p /c/ProgramData/Metaphase/ER-1/
+
+cp src/install_extras/gui/bg_img.png /c/ProgramData/Metaphase/ER-1/
+cp src/install_extras/gui/widgets.zip /c/ProgramData/Metaphase/ER-1/
